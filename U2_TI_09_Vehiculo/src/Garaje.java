f3a023268c711d6a73ca04604ae93a0c268fdf39
oumandy2106 <20243ds039@utez.edu.mx>
2025-06-18 08:25:46 -0600

    Añadi unos metodos a garage

--- U2_TI_09_Vehiculo/src/Garaje.java
+++ U2_TI_09_Vehiculo/src/Garaje.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
-
 public class Garaje {
+
+    private Vehiculo[] espacios;
+
+    private Garaje(  int numEspacios){
+        espacios = new Vehiculo[numEspacios];
+    }
+
+    public boolean estacionar(Vehiculo vehiculo){
+        for (int i = 0; i< espacios.length; i++) {
+            if(espacios == null){
+                espacios[i] = vehiculo;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean estacionar(){
+        
+    }
+
+
 }
--- U2_TI_09_Vehiculo/src/Motocicleta.java
+++ U2_TI_09_Vehiculo/src/Motocicleta.java
@@ -1,2 +1,9 @@
-public class Motocicleta {
+public class Motocicleta extends Vehiculo{
+    public boolean tieneCaja;
+
+    public Motocicleta(String placa, String marca, boolean tieneCaja){
+        super(placa, marca);
+        this.tieneCaja = tieneCaja;
+    }
+
 }
